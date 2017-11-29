1ce5f18753d7165a7f5d37be61ea07883bea454b
geistling <34081638+geistling@users.noreply.github.com>
2017-11-28 18:41:54 -0800

    Add files via upload

---
+++ batch_rename.py
@@ -0,0 +1,26 @@
+import os,glob,sys,lxml.etree
+from bs4 import BeautifulSoup as bs
+
+fstart = "/home/geistling/Documents/Aarons_Law/Lobbying_Disclosures"
+
+folders = [x[0] for x in os.walk(fstart)]
+
+for folder in folders:
+    os.chdir(folder)
+
+    files = glob.glob("*.xml")
+    print files
+    
+    for file in files:
+        data = open(file).read()
+        soup = bs(data,'xml')
+        clientName = soup.clientName.string
+        print clientName
+        reportYear = soup.reportYear.string
+        print reportYear
+        reportType = soup.reportType.string
+        print reportType
+        newfilename = '-'.join([clientName, reportYear, reportType]) + ".xml"
+        print newfilename
+        newfilename = newfilename.replace (' ', '_')
+        os.rename(file, newfilename)
