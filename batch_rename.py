acbabd7d7f71e0ddd6a766f8a871d111f2100bb1
Claire Peters <clairepeters@g.harvard.edu>
2021-09-24 12:23:04 -0700

    format intro

--- batch_rename.py
+++ batch_rename.py
@@ -1,10 +1,14 @@
-# This python script uses BeautifulSoup to batch rename .xml files based on information contained in the files.
+# This python script uses BeautifulSoup to batch rename .xml files based on
+# information contained in the files.
 # This script assumes that the .xmls being analyzed have similar formatting.
 
-# I downloaded a host of .xmls with names consisting of random numbers. This script renames them according to the 
-# contents of the files so I had an idea of who submitted the form and when it was submitted. This script reads
-# .xmls in a given directory (including subdirectory) and replaces their names with ones based on specified strings
-# in the .xml. You can alter other aspects of the new name as well - here, I replace spaces with underscores.
+# I downloaded a host of .xmls with names consisting of random numbers. 
+# This script renames them according to the  contents of the files so I had
+# an idea of who submitted the form and when it was submitted. This script
+# reads .xmls in a given directory (including subdirectory) and replaces 
+# their names with ones based on specified strings in the .xml. You can
+# alter other aspects of the new name as well - here, I replace spaces 
+# with underscores.
 
 import os
 import glob
